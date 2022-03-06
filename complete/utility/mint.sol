49fab6d7c7d44480941ba87d22fc145bc7890c1a
totodiletears <toto.ilebinted@gmail.com>
2022-03-05 18:44:49 -0600

    user array

--- complete/utility/mint.sol
+++ complete/utility/mint.sol
@@ -6,59 +6,69 @@ import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Mint is ERC1155Holder, Ownable{
+contract Mint is ERC1155Holder, Ownable {
     IERC1155 public nftToken;
     IERC20 public erc20Token;
 
+    address[] public airdropList;
+
     address collector;
-    uint remaining;
-    uint minted;
-    uint supply;
-    uint cost;
+    address nftContract;
+    uint256 remaining;
+    uint256 minted;
+    uint256 supply;
+    uint256 cost;
     bool paused;
 
     constructor(
         IERC1155 _nftToken,
-		IERC20 _erc20Token,
+        address _nftContract,
+        IERC20 _erc20Token,
         address _collector,
-        uint _supply,
-        uint _cost
+        uint256 _supply,
+        uint256 _cost
     ) {
         nftToken = _nftToken;
-		erc20Token = _erc20Token;
+        nftContract = _nftContract;
+        erc20Token = _erc20Token;
         collector = _collector;
         supply = _supply;
-        cost = _cost * 10 ** 9; // kuro
+        cost = _cost; // kuro
         paused = true;
         minted = 0;
     }
 
     // buy
 
-    function claim() public payable {
+    function mint(address _nftContract) public payable {
+        require(_nftContract == nftContract, "Wrong Contract Address");
         require(!paused, "Contract: Paused");
         require(minted + 1 <= supply, "Contract: Sold out");
-        require(erc20Token.balanceOf(msg.sender) >= cost, "Contract: You don't have enough KURO");
+        require(
+            erc20Token.balanceOf(msg.sender) >= cost,
+            "Contract: You don't have enough KURO"
+        );
         erc20Token.transferFrom(msg.sender, collector, cost);
         nftToken.safeTransferFrom(address(this), msg.sender, minted + 1, 1, "");
+        airdropList.push(msg.sender);
         minted++;
     }
 
     // misc
 
-    function nftsRemaining() public view returns (uint){
+    function nftsRemaining() public view returns (uint256) {
         return supply - minted;
     }
 
-    function setCost(uint _cost) public onlyOwner{
+    function setCost(uint256 _cost) public onlyOwner {
         cost = _cost;
     }
 
-    function getCost() public view returns (uint){
+    function getCost() public view returns (uint256) {
         return cost;
     }
 
-    function setCollector(address _collector) public onlyOwner{
+    function setCollector(address _collector) public onlyOwner {
         collector = _collector;
     }
 
@@ -66,32 +76,31 @@ contract Mint is ERC1155Holder, Ownable{
         return collector;
     }
 
-    function setSupply(uint _supply) public onlyOwner{
+    function setSupply(uint256 _supply) public onlyOwner {
         supply = _supply;
     }
 
-    function getSupply() public view returns (uint){
+    function getSupply() public view returns (uint256) {
         return supply;
     }
 
-    function setPause(bool _pause) public onlyOwner{
+    function setPause(bool _pause) public onlyOwner {
         paused = _pause;
     }
 
-    function isPaused() public view returns (bool){
+    function isPaused() public view returns (bool) {
         return paused;
     }
 
-    function totalMinted() public view returns (uint) {
+    function totalMinted() public view returns (uint256) {
         return minted;
     }
 
-    function getBalance() public view returns (uint) {
+    function getBalance() public view returns (uint256) {
         return erc20Token.balanceOf(msg.sender);
     }
 
     function getMinterAddress() public view returns (address) {
         return address(this);
     }
-
 }
